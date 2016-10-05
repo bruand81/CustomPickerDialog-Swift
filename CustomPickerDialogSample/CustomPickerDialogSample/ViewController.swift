58ca417ce8f9858030d27159d121065949e4ab42
hwj4477 <hwj4477@gmail.com>
2016-10-05 17:13:36 +0900

    apply Swift3.0

--- CustomPickerDialogSample/CustomPickerDialogSample/ViewController.swift
+++ CustomPickerDialogSample/CustomPickerDialogSample/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func clickShowDialog(sender: AnyObject) {
+    @IBAction func clickShowDialog(_ sender: AnyObject) {
         
         let pickerView = CustomPickerDialog.init()
         
